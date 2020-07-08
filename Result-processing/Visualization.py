489b6c97171e81ad38c74aa79d2174e6f5056e4e
Wayne <bwh.buaa@gmail.com>
2020-07-08 10:31:54 -0400

    Visualize the results

--- Result-processing/Visualization.py
+++ Result-processing/Visualization.py
@@ -1,21 +1,25 @@
 import graphviz
-import networkx as nx
 import extractInfo
 
 node_list, edge_list, node_type_list= extractInfo.extract('GraphRNN_RNN_AST_4_128_train_0.dat')
-print(node_list)
-print(edge_list)
-print(node_type_list)
 
-# #TESTq
-# dot = graphviz.Digraph(comment='Test')
-#
-# dot.node('A', 'Dot A')
-# dot.node('B', 'Dot B')
-# dot.node('C', 'Dot C')
-#
+dot = graphviz.Graph(comment='Result')
+
+# Add node
+flag = 0
+for i in node_list:
+    node_id = i
+    node = node_type_list[flag]
+    print(node_id, node)
+    dot.node(str(node_id), node)
+    flag += 1
+
+for i in edge_list:
+    m, n = i[0], i[1]
+    dot.edge(str(m), str(n))
+
 # dot.edges(['AB','AC','AB'])
 # dot.edge('B', 'C', 'test')
-#
-# print(dot.source)
-# dot.render('test-output/test-table.gv', view=True)
+
+print(dot.source)
+dot.render('test-output/test.gv', view=True)
