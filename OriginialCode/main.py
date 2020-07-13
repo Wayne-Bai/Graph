8af9906b11bebfc7df60bcd46697677d8e8b28c2
Wayne <bwh.buaa@gmail.com>
2020-07-13 15:28:55 -0400

    print all shape and dim

--- OriginialCode/main.py
+++ OriginialCode/main.py
@@ -103,7 +103,6 @@ if __name__ == '__main__':
         args.max_prev_node = args.max_num_node - 1
     else:
         dataset = Graph_sequence_sampler_pytorch(graphs_train,max_prev_node=args.max_prev_node,max_num_node=args.max_num_node)
-        print(dataset[0])
     sample_strategy = torch.utils.data.sampler.WeightedRandomSampler([1.0 / len(dataset) for i in range(len(dataset))],
                                                                      num_samples=args.batch_size*args.batch_ratio, replacement=True)
     dataset_loader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, num_workers=args.num_workers,
