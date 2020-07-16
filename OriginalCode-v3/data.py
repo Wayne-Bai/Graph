a77b4d8994571c8dd77cd0efe5b16622051ba853
Wayne <bwh.buaa@gmail.com>
2020-07-16 02:17:11 -0400

    change node sequence

--- OriginalCode-v3/data.py
+++ OriginalCode-v3/data.py
@@ -435,8 +435,8 @@ class Graph_sequence_sampler_pytorch(torch.utils.data.Dataset):
             [], [], [], [], []
         for i,G in enumerate(G_list):
             # add node_type_feature_matrix and edge_type_feature_matrix
-            BFS_list = bfs_seq(G)
-            self.adj_all.append(np.asarray(nx.to_numpy_matrix(G, nodelist=BFS_list)))
+            print(G.nodes(data=True))
+            self.adj_all.append(np.asarray(nx.to_numpy_matrix(G)))
             node_idx_global = np.asarray(list(G.nodes))
             self.node_num_all.append(node_idx_global)
             # print(len(G.nodes._nodes), len(G.edges._adjdict), len(list(G.adjacency())))
