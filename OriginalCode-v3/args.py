b9b89d7cb6c7d65c0ea995661263d2079dd6dda0
Wayne <bwh.buaa@gmail.com>
2020-07-16 10:23:45 -0400

    change epoch

--- OriginalCode-v3/args.py
+++ OriginalCode-v3/args.py
@@ -70,9 +70,9 @@ class Args():
         ### training config
         self.num_workers = 0 # num workers to load data, default 4
         self.batch_ratio = 4 # how many batches of samples per epoch, default 32, e.g., 1 epoch = 32 batches
-        self.epochs = 400 # now one epoch means self.batch_ratio x batch_size
+        self.epochs = 1000 # now one epoch means self.batch_ratio x batch_size
         self.epochs_test_start = 1
-        self.epochs_test = 50
+        self.epochs_test = 900
         self.epochs_log = 10
         self.epochs_save = 50
 
