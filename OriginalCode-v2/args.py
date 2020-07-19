2fd420cc1f4050bd5528ca47a35f0376a7cb59c8
Wayne <bwh.buaa@gmail.com>
2020-07-19 16:26:28 -0400

    try new dataset

--- OriginalCode-v2/args.py
+++ OriginalCode-v2/args.py
@@ -70,8 +70,8 @@ class Args():
         ### training config
         self.num_workers = 0 # num workers to load data, default 4
         self.batch_ratio = 4 # how many batches of samples per epoch, default 32, e.g., 1 epoch = 32 batches
-        self.epochs = 600 # now one epoch means self.batch_ratio x batch_size
-        self.epochs_test_start = 500
+        self.epochs = 1000 # now one epoch means self.batch_ratio x batch_size
+        self.epochs_test_start = 900
         self.epochs_test = 50
         self.epochs_log = 10
         self.epochs_save = 50
