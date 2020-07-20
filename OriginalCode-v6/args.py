dda097b5f1bdf28f4f65321a8f84fdce0ea37917
Wayne <bwh.buaa@gmail.com>
2020-07-20 15:35:51 -0400

    modify edge feature

--- OriginalCode-v6/args.py
+++ OriginalCode-v6/args.py
@@ -70,9 +70,9 @@ class Args():
         ### training config
         self.num_workers = 0 # num workers to load data, default 4
         self.batch_ratio = 4 # how many batches of samples per epoch, default 32, e.g., 1 epoch = 32 batches
-        self.epochs = 3000 # now one epoch means self.batch_ratio x batch_size
-        self.epochs_test_start = 2500
-        self.epochs_test = 100
+        self.epochs = 10 # now one epoch means self.batch_ratio x batch_size
+        self.epochs_test_start = 1
+        self.epochs_test = 1
         self.epochs_log = 10
         self.epochs_save = 2000
 
