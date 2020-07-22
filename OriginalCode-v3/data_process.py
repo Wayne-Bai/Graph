39fc72908a22a71939d3ee0d1020de3ba25e4d33
Wayne <bwh.buaa@gmail.com>
2020-07-21 23:23:10 -0400

    change edge dimension

--- OriginalCode-v3/data_process.py
+++ OriginalCode-v3/data_process.py
@@ -70,6 +70,8 @@ def Graph_load_batch(min_num_nodes = 1, max_num_nodes = 40, name = 'AST'):
 
         G_sub = G.subgraph(nodes)
 
+        print(nodes)
+
         # print(nodes)
         # print(G_sub.nodes)
         for j in range(number_of_graph_types):
