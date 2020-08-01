ae2f88a65e532b945086cb23e5e41f714c4a6d61
Wayne <bwh.buaa@gmail.com>
2020-08-01 10:07:37 -0400

    visualization on server

---
+++ Result-processing/Vis.py
@@ -0,0 +1,105 @@
+import networkx as nx
+import graphviz
+
+def extract(name, num,total_feature):
+
+    f = open('nodeLabel.txt', 'r')
+
+    G = nx.read_gpickle(name)
+
+    # print(len(G))
+    #
+    # for i in range(len(G)):
+    #     print('Graph %d'%i, G[i].number_of_nodes(), G[i].number_of_edges())
+
+    # for i in range(3):
+    #     pos = nx.spring_layout(G[i], scale=0.5)
+    #     nx.draw(G[i], pos, node_size=10, with_labels=False)
+    #     plt.savefig('fig%d.png'%i, bbox_inches='tight')
+    #     plt.show()
+
+    # Generate node list
+    node_list = G[num].node
+    print(node_list)
+    print(G[num].nodes.data())
+
+    # Generate edge list
+    edges_list = G[num].edges
+
+    # Generate node type list
+    node_type_list_num = []
+
+    print(G[num].node[0]['f0'])
+    for i in node_list:
+        for j in range(total_feature):
+            feature = 'f' + str(j)
+            print(feature)
+            if G[num].node[i][feature] == 1:
+                node_type_list_num.append(j)
+
+    print(node_type_list_num)
+    total_type = []
+    count = []
+    node_type_list = []
+
+    for line in f.readlines():
+        line = line.strip('\n')
+        type_name, type_ID = line.split(' ')
+
+        total_type.append(type_name)
+        count.append(type_ID)
+
+    for i in node_type_list_num:
+        p = count.index(str(i+1))
+        new_type = total_type[p]
+        node_type_list.append(new_type)
+
+    # Generate edge type list
+    # TODO: wait for tthe decision of edge type
+    # edge_type_list = []
+    # for i in edges_list:
+    #     m, n = i[0], i[1]
+    #     for j in range(2):
+    #         feature = 'f' + str(j+1)
+    #         if G[3][m][n] == 1:
+    #             edge_type_list.append(j+1)
+
+    return node_list, edges_list, node_type_list
+
+def Visualize(path, kind, name, num, total_feature):
+    pre_path = '/home/zfk/Documents/graph-generation/debug/Graph' + path
+    path_whole = pre_path + '/graphs/'
+    prefix = 'GraphRNN_RNN_AST%d_4_128_pred_%d_1'%(kind, name)
+    format = '.dat'
+    file = path_whole+prefix+format
+    # file = prefix+str(name)+format
+    node_list, edge_list, node_type_list = extract(file, num, total_feature)
+
+    dot = graphviz.Graph(comment='Result',format='png')
+
+    # Add node
+    flag = 0
+    print(len(node_list))
+    print(len(node_type_list))
+    for i in node_list:
+        node_id = i
+        node = node_type_list[flag]
+        print(node_id, node)
+        dot.node(str(node_id), node)
+        flag += 1
+
+    for i in edge_list:
+        m, n = i[0], i[1]
+        dot.edge(str(m), str(n))
+
+    dot.render("process/test-output/%s.gv" %(name), view=True)
+
+if __name__ == '__main__':
+
+    #TODO: this section depends on choice. (Generate all processes or only the final result)
+
+    # for i in range(31):
+    #     flag = 5000 + i*100
+    #     name = 'pred_%d_1'%(flag)
+    #     Visualize(name, 0, 28)
+    Visualize('Code-0730', 50, 3000, 0, 23)
