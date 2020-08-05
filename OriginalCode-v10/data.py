0753cc9c96939737bcee7d40fe10459339d91526
Wayne <bwh.buaa@gmail.com>
2020-08-05 15:35:22 -0400

    check edge reverse

--- OriginalCode-v10/data.py
+++ OriginalCode-v10/data.py
@@ -585,8 +585,8 @@ class Graph_sequence_sampler_pytorch(torch.utils.data.Dataset):
         raw_edge_f_batch = raw_edge_f_batch[np.ix_(x_idx, x_idx)]
         # print("raw_edge_f_batch dim: {}".format(raw_edge_f_batch.shape))
         # print('*****************************************')
-        # print(raw_edge_f_batch)
-        # print('*****************************************')
+        print(raw_edge_f_batch)
+        print('*****************************************')
         # print('----------------------------------------------')
         edge_f_encoded = encode_adj(raw_edge_f_batch.copy(), max_prev_node=self.max_prev_node, is_3D=True) # Dim: N * M * EF
         # print("edge_f_encoded: {}".format(edge_f_encoded))
