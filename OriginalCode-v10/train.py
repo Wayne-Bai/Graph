308d30c7530654edb31cb8dd2cb5a45a8054347e
Wayne <bwh.buaa@gmail.com>
2020-08-05 15:31:44 -0400

    check edge reverse

--- OriginalCode-v10/train.py
+++ OriginalCode-v10/train.py
@@ -459,6 +459,9 @@ def train_rnn_epoch(epoch, args, rnn, output, data_loader,
         input_node_f_unsorted = data['input_node_f'].float() # Dim: BS * N_max * INF
         raw_node_f_unsorted = data['raw_node_f'].float() # Dim: BS * N_max * NF
         edge_f_unsorted = data['edge_f'].float() # Dim: BS * N_max * M * EF
+
+        print('--------------------edge f unsorted------------------')
+        print(edge_f_unsorted)
         y_len_unsorted = data['len'] # list of node numbers in each graph in this batch
         y_len_max = max(y_len_unsorted) # denote as N
         # x_unsorted = x_unsorted[:, 0:y_len_max, :]# Dim: BS * N * M
@@ -480,8 +483,8 @@ def train_rnn_epoch(epoch, args, rnn, output, data_loader,
         raw_node_f = torch.index_select(raw_node_f_unsorted, 0, sort_index)
         edge_f = torch.index_select(edge_f_unsorted, 0, sort_index)
 
-        print('----------------------edge f: 1 ----------------------')
-        print(edge_f)
+        # print('----------------------edge f: 1 ----------------------')
+        # print(edge_f)
 
 
         # input, output for output rnn module
@@ -489,8 +492,8 @@ def train_rnn_epoch(epoch, args, rnn, output, data_loader,
         # y_reshape = pack_padded_sequence(y,y_len,batch_first=True).data # Dim: SumN * M
         # input should be edge_f, output should be dim: SumN * M * EF
         edge_f_reshape = pack_padded_sequence(edge_f,y_len,batch_first=True).data # SumN * M * EF
-        print('----------------------edge f reshape: 2 ----------------------')
-        print(edge_f_reshape)
+        # print('----------------------edge f reshape: 2 ----------------------')
+        # print(edge_f_reshape)
 
         # # reverse y_reshape, so that their lengths are sorted, add dimension
         # idx = [i for i in range(y_reshape.size(0)-1, -1, -1)]
@@ -501,18 +504,18 @@ def train_rnn_epoch(epoch, args, rnn, output, data_loader,
         # output_x = torch.cat((torch.ones(y_reshape.size(0),1,1),y_reshape[:,0:-1,0:1]),dim=1) # should have all-1 row
         # reverse edge_f_reshape, so that their lengths are sorted, add dimension
         idx = [i for i in range(edge_f_reshape.size(0) - 1, -1, -1)]
-        print('----------------------idx----------------------')
-        print(idx)
+        # print('----------------------idx----------------------')
+        # print(idx)
         idx = torch.LongTensor(idx)
         edge_f_reshape = edge_f_reshape.index_select(0, idx)
         edge_f_reshape = edge_f_reshape.view(edge_f_reshape.size(0), edge_f_reshape.size(1), edge_f_reshape.size(2))  # Dim: SumN * M * EF
-        print('----------------------edge f reshape: 3 ----------------------')
-        print(edge_f_reshape)
+        # print('----------------------edge f reshape: 3 ----------------------')
+        # print(edge_f_reshape)
         edge_rnn_input = torch.cat((torch.ones(edge_f_reshape.size(0), 1, edge_f_reshape.size(2)), edge_f_reshape[:, 0:-1, :]),
                              dim=1)  # should have all-1 row
         # Dim: SumN * (M+1) * EF
-        print('----------------------edge f reshape: 4 ----------------------')
-        print(torch.ones(edge_f_reshape.size(0), 1, edge_f_reshape.size(2)), edge_f_reshape[:, 0:-1, :])
+        # print('----------------------edge f reshape: 4 ----------------------')
+        # print(torch.ones(edge_f_reshape.size(0), 1, edge_f_reshape.size(2)), edge_f_reshape[:, 0:-1, :])
 
         # output_y = y_reshape # Dim: SumN * M * 1
         output_y = edge_f_reshape
