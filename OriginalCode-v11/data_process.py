77665f0350a525820538316446053cd4ebafe318
Wayne <bwh.buaa@gmail.com>
2020-08-10 16:46:52 -0400

    add new dataset address

--- OriginalCode-v11/data_process.py
+++ OriginalCode-v11/data_process.py
@@ -20,7 +20,7 @@ def Graph_load_batch(min_num_nodes = 1, max_num_nodes = 300, name = 'AST'):
     elif args.dataset_type == "50-200":
         path = "../dataset/dataset_50graphs_200nodes_25features/"
     elif args.dataset_type == '50-10':
-        path = '../dataset/dataset_50graphs_10nodes/'
+        path = '../../../dataset/dataset_50graphs_10nodes/'
     
 
     data_adj = np.loadtxt(path + name + '_A.txt', delimiter=',').astype(int)
