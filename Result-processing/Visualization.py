5354fc4d8b8724e19fc28159fbccaea8097bc64d
Wayne <bwh.buaa@gmail.com>
2020-08-14 00:16:22 -0400

    visualize graphs with node value

--- Result-processing/Visualization.py
+++ Result-processing/Visualization.py
@@ -2,25 +2,30 @@ import graphviz
 import extractInfo
 
 def Visualize(name, num, total_feature):
-    # path = 'graphs15/'
-    prefix = 'GraphRNN_RNN_AST50_4_128_'
+    # path = 'result2/'
+    prefix = 'GraphRNN_RNN_AST2-normalize_4_128_'
     # prefix = 'GraphRNN_RNN_AST_4_128_'
     format = '.dat'
     # file = path+prefix+str(name)+format
     file = prefix+str(name)+format
-    node_list, edge_list, node_type_list = extractInfo.extract(file, num, total_feature)
+    node_list, edge_list, node_type_list, node_value_list, node_value_type_list \
+        = extractInfo.extract(file, num, total_feature)
 
     dot = graphviz.Graph(comment='Result')
 
     # Add node
     flag = 0
-    print(len(node_list))
-    print(len(node_type_list))
+    print(node_list)
+    # print(len(node_list))
+    # print(len(node_type_list))
+    print(node_type_list)
     for i in node_list:
         node_id = i
+        # print(node_id)
         node = node_type_list[flag]
+        node_value = node_value_list[flag]
         print(node_id, node)
-        node_p = '[{}]'.format((node_id)) +': ' + node
+        node_p = '[{}]'.format((node_id)) +': ' + node + '\n' + '[{}]'.format(node_value)
         dot.node(str(node_id), node_p)
         flag += 1
 
@@ -31,9 +36,8 @@ def Visualize(name, num, total_feature):
     dot.render("process/test-output/%s.gv" %(file), view=True)
 
 if __name__ == '__main__':
-    # for i in range(31):
-    #     flag = 5000 + i*100
-    #     name = 'pred_%d_1'%(flag)
-    #     Visualize(name, 0, 28)
-    Visualize('pred_5000_1',15,28)
+    # for i in range(2000, 3100,100):
+    #     name = 'pred_%d_1'%(i)
+    #     Visualize(name, 0, 43)
+    Visualize('pred_3000_1',1,43)
 
--- Result-processing/extractInfo.py
+++ Result-processing/extractInfo.py
@@ -3,6 +3,7 @@ import networkx as nx
 def extract(name, num,total_feature):
 
     f = open('nodeLabel.txt', 'r')
+    f1 = open('nodeValue_2.txt', 'r')
 
     G = nx.read_gpickle(name)
 
@@ -19,24 +20,50 @@ def extract(name, num,total_feature):
 
     # Generate node list
     node_list = G[num].node
+    node_list_dic = dict(G[num].nodes._nodes)
+    node_feature = list(next(iter(node_list_dic.values())).keys())
     print(node_list)
     print(G[num].nodes.data())
+    print(type(node_list_dic))
 
     # Generate edge list
     edges_list = G[num].edges
 
     # Generate node type list
     node_type_list_num = []
-
+    node_value_type_list =[]
+    node_value = []
 
     for i in node_list:
-        for j in range(total_feature):
-            feature = 'f' + str(j)
-            print(feature)
-            if G[num].node[i][feature] == 1:
-                node_type_list_num.append(j)
+        for j in node_feature:
+            if 'type' in j and 'value' not in j:
+                if G[num].node[i][j] == 1:
+                    m, n = j.split('e')
+                    node_type_list_num.append(n)
+            elif 'type' in j and 'value' in j:
+                if G[num].node[i][j] == 1:
+                    m, n = j.split('type')
+                    node_value_type_list.append(n)
+            elif 'int' in j and 'value' in j:
+                if G[num].node[i]['value_type0'] == 1:
+                    node_value.append(G[num].node[i][j])
+            elif 'float' in j and 'value' in j:
+                if G[num].node[i]['value_type1'] == 1:
+                    node_value.append(G[num].node[i][j])
+            elif 'string' in j and 'value' in j:
+                if G[num].node[i][j] == 1:
+                    m, n = j.split('value')
+                    node_value.append(n)
+    # print(node_type_list_num)
+    # print(node_value_type_list)
+    # print(node_value)
+        # for j in range(total_feature):
+        #     feature = 'f' + str(j)
+        #     print(feature)
+        #     if G[num].node[i][feature] == 1:
+        #         node_type_list_num.append(j)
 
-    print(node_type_list_num)
+    # print(node_type_list_num)
     total_type = []
     count = []
     node_type_list = []
@@ -49,9 +76,23 @@ def extract(name, num,total_feature):
         count.append(type_ID)
 
     for i in node_type_list_num:
-        p = count.index(str(i+1))
+        p = count.index(str(int(i)+1))
         new_type = total_type[p]
         node_type_list.append(new_type)
+
+    total_value = []
+    count_v = []
+    node_value_list = []
+    for line in f1.readlines():
+        line = line.strip('\n')
+        value_id, value_name = line.split(': ')
+        total_value.append(value_name)
+        count_v.append(value_id)
+    for i in node_value:
+        p = count_v.index(str(int(i)+1))
+        new_value = total_value[p]
+        node_value_list.append(new_value)
+    # print(node_value_list)
 
     # Generate edge type list
     # TODO: wait for tthe decision of edge type
@@ -63,4 +104,4 @@ def extract(name, num,total_feature):
     #         if G[3][m][n] == 1:
     #             edge_type_list.append(j+1)
 
-    return node_list, edges_list, node_type_list
+    return node_list, edges_list, node_type_list, node_value_list, node_value_type_list
